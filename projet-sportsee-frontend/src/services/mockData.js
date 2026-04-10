62bf089cc2362162d3a12cd253d370134c3fed45
Mireille Emonot <130974267+MireilleEM@users.noreply.github.com>
2026-04-10 16:20:25 +0200

    premieres infos mock

---
+++ projet-sportsee-frontend/src/services/mockData.js
@@ -0,0 +1,18 @@
+// src/data/mockData.js
+export const USER_INFO_MOCK = [
+    { id: "user123", profile: {
+        "firstName": "Sophie",
+        "lastName": "Martin",
+        "createdAt": "2025-01-01",
+        "age": 32,
+        "weight": 60,
+        "height": 165,
+        "profilePicture": "http://localhost:8000/images/sophie.jpg"
+    }, statistics: {
+        "totalDistance": "2250.2",
+        "totalSessions": 348,
+        "totalDuration": 14625
+    }},
+   // { id: "user456", profile: { firstName: "Marc", ... } },
+   // { id: "user789", profile: { firstName: "Emma", ... } }
+];
